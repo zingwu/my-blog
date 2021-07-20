59b77ccfc26d390748ee0fcf781cb966fc9f659c
zingwu <14780023115@163.com>
2021-07-20 17:25:47 +0800

    fix: change logo require path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@
 import React, { FC } from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Zing from '@site/static/img/default-monochrome.svg';
 import { Quote } from '../components';
 
 const Home:FC = () => {
@@ -14,11 +15,9 @@ const Home:FC = () => {
       isIndex
     >
       <img src="https://api.ixiaowai.cn/gqapi/gqapi.php" />
-      <img
-        src="static/img/default-monochrome.svg"
-        style={{
-          position: 'absolute', top: 400, left: 600, width: 500,
-        }}
+      <Zing style={{
+        position: 'absolute', top: 400, left: 600, width: 500,
+      }}
       />
       <Quote />
     </Layout>
